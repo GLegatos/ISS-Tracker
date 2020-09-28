34fdcaa66a2d4fc0bd388ee466f3a8b10a484d16
GLegatos <GLegatos@gmail.com>
2020-09-28 10:08:32 -0400

    Updates

---
+++ precache-manifest.7926991352cb605d36a0745d26ed9eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2de01a0db18596d1cafdc6b761efb6",
+    "url": "/ISS-Tracker/index.html"
+  },
+  {
+    "revision": "56f70b1cc942c9ca8b74",
+    "url": "/ISS-Tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f70a7e65276c447e198",
+    "url": "/ISS-Tracker/static/js/2.d0467736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ISS-Tracker/static/js/2.d0467736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f70b1cc942c9ca8b74",
+    "url": "/ISS-Tracker/static/js/main.bc824ab7.chunk.js"
+  },
+  {
+    "revision": "887bc337eb3784b1ba5d",
+    "url": "/ISS-Tracker/static/js/runtime-main.a7466a15.js"
+  }
+]);
